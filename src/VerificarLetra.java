8ecdc7306d0df964a1f4bae23cb43547eaaa5556
Pablo <pabmoranc@alu.edu.gva.es>
2024-12-10 16:05:52 +0100

    Delete src directory

--- src/VerificarLetra.java
+++
@@ -1,9 +0,0 @@
-public class VerificarLetra {
-    public static boolean verificarLetra(String XXX, char XXX, StringBuilder letrasAdivinadas) {
-        if (palabra.indexOf(XXX) >= 0) {
-            letrasAdivinadas.append(XXX);
-            return XXX;
-        }
-        return XXX;
-    }
-}
